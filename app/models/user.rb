fd722c350d3f76b68128a0aaac0ba0a667d1028d
Che Wang <chewang95@aol.com>
2017-09-29 15:48:47 -0400

    End of Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,18 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+    # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+
 
     private
 
